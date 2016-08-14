611de717c3950d407b1cc5c4a023104673b08207
ElFabricio <Fabrice911@web.de>
2016-08-14 18:00:21 +0200

    -Added new animations for Ferroda -Localized all Hero Weapons -Attempted to fix Gunship (Republic). Collisions are fixed; however these turret balls are still a pain for me -Minor changed to heroes -Improved Block2 -Fixed Aayla's Lightsaber wielding

--- Animations/SoldierAnimationBank/Padme/munge.bat
+++ Animations/SoldierAnimationBank/Padme/munge.bat
@@ -1,4 +1,4 @@
 @set path=C:/windows/system32
 
 
-@call ..\munge_animation.bat "/keepframe0 /rootscale DummyRoot 0.85 /dest padme.zaf" Sides\Heroes
+@call ..\munge_animation.bat "/keepframe0 /dest padme.zaf" Sides\Heroes
